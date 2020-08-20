5a1822591ad6e482ddecf088ac574f2ab724f9e2
Jason Markson <jasonmarkson76@gmail.com>
2020-08-20 01:10:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
-def unsafe?(speed)
-
+def unsafe? (speed)
+  if speed < 40
+    return true
+  elsif speed > 60 
+    return true
+  else 
+    false
+  end
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe? (speed)
+  speed > 60 ? true : false
+  speed < 40 ? true : false
 end
-	
-
+  
 
